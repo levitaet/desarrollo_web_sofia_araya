d77ad2e6a9accf144025200d42346747580d23be
levitaet <sofia.araya.v@ug.uchile.cl>
2026-09-07 21:24:35 -0300

    refactor: data folder

---
+++ assets/js/data/tipos-aves.js
@@ -0,0 +1,37 @@
+
+var TiposDeAves = {
+
+    "tipos": [{
+        "TipoAve": "Rapaz",
+        "Subdivision": ["Diurna", "Nocturna"]
+    },
+    {
+        "TipoAve": "Acuática",
+        "Subdivision": []
+    },
+    {
+        "TipoAve": "Marina",
+        "Subdivision": ["Oceánica", "Costera"]
+    },
+    {
+        "TipoAve": "Paseriforme",
+        "Subdivision": []
+    },
+    {
+        "TipoAve": "Zancudas",
+        "Subdivision": ["De humedal", "De ribera"]
+    },
+    {
+        "TipoAve": "Trepadora",
+        "Subdivision": []
+    },
+    {
+        "TipoAve": "Corredora",
+        "Subdivision": []
+    },
+    {
+        "TipoAve": "Psitácida",
+        "Subdivision": ["Frugívora", "Granívora"]
+    },
+    ]
+};
